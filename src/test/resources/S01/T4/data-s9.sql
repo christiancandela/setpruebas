4199e435334bef4194a3333cb95af0cc5e438359
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2023-07-14 17:48:19 -0500

    Se publican los set de pruebas

---
+++ src/test/resources/S01/T4/data-s9.sql
@@ -0,0 +1,2 @@
+insert into Owner values (1,"etai9","manager1");
+insert into ResourceItem values(1,"resource1","generic","enabled",now(),1);
---
+++ src/test/resources/S01/T8/data-s2.sql
@@ -0,0 +1,2 @@
+insert into Owner values (1,"etai2","manager1");
+insert into ResourceItem values(1,"resource1","generic","enabled",now(),1);
